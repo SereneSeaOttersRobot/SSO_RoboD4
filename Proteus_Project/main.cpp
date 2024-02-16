91bf71729ef2103be1f7f6c27a13db30b7161a4b
L0g5n <roberts.2366@osu.edu>
2024-02-16 11:46:37 -0500

    main for cds sensor test.

--- Proteus_Project/main.cpp
+++ Proteus_Project/main.cpp
@@ -1,8 +1,14 @@
 #include <FEHLCD.h>
+#include <FEHIO.h>
 
 int main(void)
 {
     LCD.Clear(BLACK);
     LCD.WriteLine("Hello, World!");
+    AnalogInputPin cds(FEHIO::P0_0);
+    while(1){
+        LCD.Clear(BLACK);
+        LCD.WriteLine(cds.Value());
+    }
 	return 0;
 }
