9f37afbdd9dbc8fd06466541889e7a959f626ecc
L0g5n <roberts.2366@osu.edu>
2024-02-16 12:45:26 -0500

    Light sensor control servor maked

--- Proteus_Project/main.cpp
+++ Proteus_Project/main.cpp
@@ -1,15 +1,21 @@
+#include <FEHUtility.h>
+#include <FEHServo.h>
 #include <FEHLCD.h>
 #include <FEHIO.h>
 
+FEHServo servo(FEHServo::Servo7); // declare servo arm
 int main(void)
 {
-    LCD.Clear(BLACK);
-    LCD.WriteLine("Hello, World!");
+    servo.SetMax(2262);
+    servo.SetMin(890);
     AnalogInputPin cds(FEHIO::P0_0);
     while(1){
-        Sleep(0.5);
+        float angle = cds.Value() * 54.54;
+        servo.SetDegree(angle);
         LCD.Clear(BLACK);
-        LCD.WriteLine(cds.Value());
+        LCD.WriteLine(angle);
+        Sleep(0.2);
     }
-	return 0;
+    
+    
 }
