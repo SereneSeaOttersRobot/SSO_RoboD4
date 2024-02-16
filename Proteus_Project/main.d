88d3bc9ff21e4c03c08c8db7ea36b4ad6702b793
reillybosse <bosse.48@osu.edu>
2024-02-16 11:53:48 -0500

    Commmit fixe?

---
+++ Proteus_Project/main.d
@@ -0,0 +1,13 @@
+../main.d: ../main.cpp Libraries/FEHLCD.h Drivers/spi.h \
+ ../fehproteusfirmware/Startup/derivative.h Startup/MK60DZ10.h \
+ Libraries/FEHUtility.h Startup/derivative.h Libraries/LCDColors.h \
+ Libraries/FEHIO.h Drivers/adc16.h
+Libraries/FEHLCD.h:
+Drivers/spi.h:
+../fehproteusfirmware/Startup/derivative.h:
+Startup/MK60DZ10.h:
+Libraries/FEHUtility.h:
+Startup/derivative.h:
+Libraries/LCDColors.h:
+Libraries/FEHIO.h:
+Drivers/adc16.h:
