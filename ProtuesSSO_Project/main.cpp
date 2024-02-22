dc88662dda56e614ae375cfe56dff50f8221f257
L0g5n <roberts.2366@osu.edu>
2024-02-22 18:26:24 -0500

    Functions added, main replaced, global variables added implementations limited

--- ProtuesSSO_Project/main.cpp
+++ ProtuesSSO_Project/main.cpp
@@ -6,59 +6,125 @@
 #include <FEHAccel.h>
 #include <FEHSD.h>
 
-FEHMotor leftVex(FEHMotor::Motor1, 1.0);
-FEHMotor rightVex(FEHMotor::Motor2, 1.0);
-AnalogInputPin cds1(FEHIO::P0_0);
-FEHServo liftServo(FEHServo::FEHServoPort);
-AnalogInputPin optoLine1(FEHIO::P0_1);
-AnalogInputPin optoLine2(FEHIO::P0_1);
-AnalogInputPin optoLine3(FEHIO::P0_1);
+
+/*
+Function prototypes and contracts
+*/
+
+float turn(float degrees, int timeLimit);
+
+float move(float inches, int timeLimit);
+
+bool withinStartLightRange(float lightValue);
+
+bool withinTicketLightRange(float lightValue);
+
+/*
+Global declarations of hardware objects
+*/
+//Motors
+const static float motor_voltage = 1.0;
+FEHMotor motor_left(FEHMotor::Motor0, motor_voltage);
+FEHMotor motor_right(FEHMotor::Motor1, motor_voltage);
+//*************** Sensors ***************
+//******analog*******
+//light color sensor (cds)
+const static float sensor_cds_voltage = 3.3;
+AnalogInputPin sensor_cds(FEHIO::P0_0);
+
+//******digital*******
+DigitalInputPin sensor_bumper_pos1(FEHIO::P0_1);
+DigitalInputPin sensor_bumper_pos2(FEHIO::P0_2);
+DigitalInputPin sensor_bumper_pos3(FEHIO::P0_3);
+//...
 
 
+/*
+Global declarations for non-hardware variables.
+*/
 
+//start light
+float start_light = 0;
 
 int main(void)
-{
-    //AnalogInputPin cds(FEHIO::P0_0);
-    FEHMotor lefDr(FEHMotor::Motor2, 9.0);
-    FEHMotor rigDr(FEHMotor::Motor0, 9.0);
-    DigitalInputPin microFL(FEHIO::P3_7);
-    DigitalInputPin microFR(FEHIO::P0_2);
-    DigitalInputPin microBL(FEHIO::P3_5);
-    DigitalInputPin microBR(FEHIO::P0_4);
-    float x, y;
-    LCD.ClearBuffer();
-    while(!LCD.Touch(&x,&y));
-    while(LCD.Touch(&x,&y));
-    //Part 1
-    lefDr.SetPercent(24.);
-    rigDr.SetPercent(25.);
-    while(microFL.Value() || microFR.Value());
-    lefDr.Stop();
-    rigDr.Stop();
-    //Part 2
-    rigDr.SetPercent(-30.);
-    lefDr.SetPercent(5.);
-    while(microBL.Value() || microBR.Value());
-    lefDr.Stop();
-    rigDr.Stop();
-    //Part 3
-    lefDr.SetPercent(24.);
-    rigDr.SetPercent(25.);
-    while(microFL.Value() || microFR.Value());
-    lefDr.Stop();
-    rigDr.Stop();
-    //Part 4..
-    lefDr.SetPercent(-30.);
-    rigDr.SetPercent(5.);
-    while(microBL.Value() || microBR.Value());
-    lefDr.Stop();
-    rigDr.Stop();
-    //Part 5
-    lefDr.SetPercent(24.);
-    rigDr.SetPercent(25.);
-    while(microFL.Value() || microFR.Value());
-    lefDr.Stop();
-    rigDr.Stop();    
-    
+{   
+    //start
+    LCD.Clear(BLACK);
+    LCD.WriteLine("Touch to ready up");
+    //Wait on touch
+    float *x, *y;
+    while(!LCD.Touch(x,y));
+    while(LCD.Touch(x,y));
+
+    //wait for acceptable value from light sensor
+    int updateWaitTicks = 10, ticks = 10;
+    float lightValue;
+    do{
+        lightValue = sensor_cds.Value();
+        if (ticks == updateWaitTicks){
+            LCD.Clear(BLACK);
+            LCD.WriteLine("detected light value");
+            LCD.WriteLine(lightValue);
+            ticks = 0;
+        }
+        ticks++;
+    }while(!withinStartLightRange(lightValue));
+    LCD.Clear(BLACK);
+    LCD.WriteLine("detected and saved light value");
+    LCD.WriteLine(lightValue);
+    start_light = lightValue;
+
+    /*
+    Main function constants
+    */
+    const int time_limit = 2; // 2 second time limit base
+    //********
+
+    //**ISSUE** floater math is not exact, == or != is bad when comparing floats, high chance of error
+
+    //Turn and move to ramp
+    float turn1 = 45.0;
+    float turned1 = turn(turn1, time_limit);
+    if (turn1 != turned1){
+        //error handling
+    }
+    float move1 = 12.0; //inches
+    float moved1 = move(move1, time_limit);
+    if (move1 != moved1){
+        //error handling
+    }
+
+
+
+}
+
+float turn(float degrees, int timeLimit){
+    //here for compiler error handling, replace with actual implementation.
+    float res = 0.0;
+    //remove full revolutions from turn.
+    int numRevInt = ((int)degrees)/360;
+    const float desired_degrees = degrees - (360*numRevInt);
+
+    return res;
+}
+
+float move(float inches, int timeLimit){
+    //here for compiler error handling, replace with actual implementation.
+    float res = 0.0;
+
+    return res;
+}
+
+bool withinStartLightRange(float lightValue){
+    //here for compiler error handling, replace with actual implementation.
+    bool res = false;
+
+    return res;
+}
+
+bool withinTicketLightRange(float lightValue){
+    //here for compiler error handling, replace with actual implementation.
+    bool res = false;
+
+    return res;
 }
