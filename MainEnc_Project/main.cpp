b42d77f9372360375065f6c5c561a9341a88d10f
L0g5n <roberts.2366@osu.edu>
2024-03-06 04:21:14 -0500

    move, turnOne, turnBoth, reportAngle methods done

--- MainEnc_Project/main.cpp
+++ MainEnc_Project/main.cpp
@@ -18,7 +18,36 @@
 #define HIGH_THRESHOLD 2.25
 //one tick is 0.07854
 #define TICKS_INCHES 0.07854
+#define ONE_TICK_DIF_DEGREE 0.615 /* - /\ + */
 #define CDS_SENSOR_PORT FEHIO::P0_2
+
+
+/**
+ * @brief reports the current angle of the robot with a error of + or -1 degree
+ * @return double degrees between -179.9 and 179.9
+ */
+double reportAngle();
+
+/**
+ * @brief uses both motors to turn the argued amount of degrees, less percise but turn on the spot. - turn left, + turn right. Also adjusts setDir for encoders (reset by end).
+ * @param degrees number of degrees to turn, error can range from + or - 2 degrees. Don't send more than 180 in either direction
+ * @return the returned value of reportAngle at the end of this.
+ */
+double turnBoth(double degrees);
+
+/**
+ * @brief uses one motor to turn the argued amount of degrees, more percise but wider turn. - turn left, + turn right
+ * @param degrees number of degrees to turn, error can range from + or - 1 degrees. Don't send more than 180 in either direction
+ * @return the returned value of reportAngle at the end of this.
+ */
+double turnOne(double degrees);
+
+/**
+ * @brief linear movement for the argued number of inches, - backwards + forwards, with in 0.1 inches. IMPT encoders are tick reset. Also adjusts setDir for encoders (reset by end)
+ * @param inches the number of inches to move
+ * @return the actual number of inches moved
+ */
+double move(double inches);
 
 
 /*
@@ -111,3 +140,83 @@ int main(){
     */
 }
 
+double reportAngle(){
+    double res = cody.getRightDifferLeft() * ONE_TICK_DIF_DEGREE;
+    //linear time, find better way to do it later
+    while(res <= -180.0 || res >= 180.0){
+        if (res <= -180.0){
+            res += 360;
+        } else {
+            res -= 360;
+        }
+    }
+    return res;
+}
+
+double turnBoth(double degrees){
+    double curDeg = reportAngle();
+    double getToDeg = curDeg + degrees;
+    if (getToDeg <= -180.0){
+        getToDeg += 360.0;
+    } else if (getToDeg >= 180.0){
+        getToDeg -= 360.0;
+    }
+    if (degrees < 0){
+        cody.setDir(false, true);
+        moto.setPerc(-15.0,15.0);
+    } else {
+        cody.setDir(true,false);
+        moto.setPerc(15.0,-15.0);
+    }
+    const float tolerance = 1.4;
+    do {
+        curDeg = reportAngle();
+    } while (getToDeg - tolerance <= curDeg && getToDeg <= getToDeg + tolerance);
+    moto.stop();
+    cody.setDir(true,true);
+    //end of turnBoth
+    return reportAngle();
+}
+
+double turnOne(double degrees){
+    double curDeg = reportAngle();
+    double getToDeg = curDeg + degrees;
+    if (getToDeg <= -180.0){
+        getToDeg += 360.0;
+    } else if (getToDeg >= 180.0){
+        getToDeg -= 360.0;
+    }
+    if (degrees < 0){
+        moto.setPerc(0,15.0);
+    } else {
+        moto.setPerc(15.0,0);
+    }
+    const float tolerance = 0.7;
+    do {
+        curDeg = reportAngle();
+    } while (getToDeg - tolerance <= curDeg && getToDeg <= getToDeg + tolerance);
+    moto.stop();
+    //end of turnOne
+    return reportAngle();
+}
+
+double move(double inches){
+    double actualInches = 0;
+    cody.resetTicks();
+    int lefTic, rigTic;
+    const int tolerance = 0.1;
+    if (inches < 0){
+        cody.setDir(false, false);
+        moto.setPerc(-15.0);
+    } else {
+        cody.setDir(true, true);
+        moto.setPerc(15.0);
+    }
+    do {
+        cody.ticks(lefTic, rigTic);
+        actualInches = ((lefTic+rigTic)/2 ) * TICKS_INCHES;
+    } while (inches - tolerance <= actualInches && actualInches <= inches + tolerance );
+    moto.stop();
+    cody.setDir(true,true);
+    return actualInches;
+}
